4c677c03e4465d8a99ac6e58c47867991fc20e84
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-08-31 23:03:46 +0600

    Some Dummy Text Removed

--- js/app.js
+++ js/app.js
@@ -33,12 +33,10 @@ const displayPhones = (phones, phoneLimit) => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-                    <div class="card p-5 ">
+                    <div class="card p-5">
                         <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${phone.phone_name}</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural
-                                lead-in to additional content. This content is a little bit longer.</p>
                             <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetaisModal">Details</button>
                         </div>
                     </div>
@@ -74,7 +72,6 @@ document.getElementById('btn-search').addEventListener('click', function () {
 });
 
 document.getElementById("inputField").addEventListener("keypress", function (event) {
-    console.log(event.key)
     if (event.key === "Enter") {
         showPhone(10);
     }
@@ -92,7 +89,7 @@ const loadPhoneDetails = id => {
 }
 
 const showPhoneDetailsModal = phone => {
-    console.log(phone);
+    // console.log(phone);
     const modalTitle = document.getElementById('phoneDetaisModalLabel');
     modalTitle.innerText = phone.name;
     const modalBodyDetais = document.getElementById('modalBody');
